c72ba79f4c133c66546d2948c2d20d131da8ae3e
Guille <guillelo11@gmail.com>
2018-02-15 23:32:00 +0100

    Add init test case

---
+++ test/tests.py
@@ -0,0 +1,12 @@
+import unittest, json
+from ethplorer.address import Address
+
+class TestAddessRequest(unittest.TestCase):
+    ADDRESS = '0x48c80f1f4d53d5951e5d5438b54cba84f29f32a5'
+
+    def test_get_address_info(self):
+        r = Address(address=self.ADDRESS)
+        self.assertEqual(json.loads(r.get_address_info())['address'], self.ADDRESS)    
+
+if __name__ == '__main__':
+    unittest.main()
