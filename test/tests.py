68c7f7e47170da50fc575423b34ad451c6c8738a
Guille <guillelo11@gmail.com>
2018-02-15 23:53:21 +0100

    Remove json import

--- test/tests.py
+++ test/tests.py
@@ -1,12 +1,13 @@
-import unittest, json
+import unittest
 from ethplorer.address import Address
 
 class TestAddessRequest(unittest.TestCase):
+
     ADDRESS = '0x48c80f1f4d53d5951e5d5438b54cba84f29f32a5'
 
     def test_get_address_info(self):
         r = Address(address=self.ADDRESS)
-        self.assertEqual(json.loads(r.get_address_info())['address'], self.ADDRESS)    
+        self.assertEqual(r.get_address_info()['address'], self.ADDRESS)
 
 if __name__ == '__main__':
     unittest.main()
